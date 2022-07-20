d73a93016ce47c2d9736e0b798f904132c030fab
Mikhail Belenko <mefisto40000x@gmail.com>
2022-07-20 13:54:47 +0300

    Fix network response Close socket with deinit

---
+++ FenestramMessanger/Core/Models/BaseResponseEntity.swift
@@ -0,0 +1,22 @@
+//
+//  BaseResponseEntity.swift
+//  FenestramMessanger
+//
+//  Created by Михаил Беленко on 20.07.2022.
+//
+
+import Foundation
+
+public struct BaseResponseEntity<T: Codable>: Codable {
+    public let data: T?
+    public let error: ErrorEntity?
+}
+
+public struct ErrorEntity: Codable, LocalizedError {
+    public let code: Int
+    public let message: String
+    
+    public var errorDescription: String? {
+        return message
+    }
+}
